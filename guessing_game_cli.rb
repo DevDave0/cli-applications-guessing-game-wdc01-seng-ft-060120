3229206bdd087008ff8c4d31ab87a6f609687dd3
David Chung <dbchung3@yahoo.com>
2020-04-22 13:09:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,3 +14,18 @@ def run_guessing_game
     puts "Goodbye!"
   
 end 
+
+def practice
+  puts "Guess a number between 1 and 6"
+  computer = (rand(6)+1).to_s 
+  input = gets.chomp 
+  
+  if input == computer 
+    puts "You guessed the correct number!" 
+  elsif input == "exit"
+  puts "Goodbye!"
+else 
+  puts "Sorry! The computer guessed #{computer}."
+end 
+  
+end 

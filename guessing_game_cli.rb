27c1a3f4cdca59caf7961394e7c79a32d5b00fa3
David Chung <dbchung3@yahoo.com>
2020-04-22 13:10:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,18 +2,6 @@
 
 require 'pry'
 def run_guessing_game
-   puts "Guess a number between 1 and 6"
-  computer_guess = rand(1..6).to_s
-  user_input = gets.chomp
-  
-  while user_input != "exit" do 
-    if user_input == computer_guess
-      puts "You guessed the correct number!"
-    else 
-      puts "Sorry! The computer guessed #{computer_guess}."
-    end 
-  end 
-    puts "Goodbye!"
   
 end 
 
@@ -31,3 +19,5 @@ else
 end 
   
 end 
+
+binding.pry
